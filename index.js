5ed178d100dff201f04be6f389330ba3726bf526
unknown <rohanbadgujar2001@gmail.com>
2024-08-19 15:54:48 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const connectDB = require("./db.js");
+const cors = require("cors");
+const http = require("http");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.use("/user", require("./Routes/auth_routes.js"));
+app.use("/message", require("./Routes/message_routes.js"));
+app.use("/conversation", require("./Routes/conversation_routes.js"));
+
+// Server setup
+const server = http.createServer(app);
+
+// Socket.io setup
+require("./socket.js")(server); // Initialize socket.io logic
+
+// Start server and connect to database
+server.listen(PORT, () => {
+  console.log(`🚀 Server started at http://localhost:${PORT}`);
+  connectDB();
+});
